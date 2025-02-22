cdeb1f196b57da79068d405dfac1982ad289896f
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2025-02-22 18:41:30 +0600

    [feat]: update logo component and enhance Home page layout

--- popup/components/Logo.tsx
+++ popup/components/Logo.tsx
@@ -1,18 +1,21 @@
-import railwayLogo from "../assets/bangladesh-railway.png";
+import bangladeshRailwayLogo from "../assets/bangladesh-railway.png";
 
 export default function Logo() {
 	return (
 		<>
 			<div className="flex flex-col">
 				<div className="flex items-center justify-center">
-					<img src={railwayLogo} />
+					<img src={bangladeshRailwayLogo} alt="Bangladesh Railway Logo" />
 				</div>
-				<div className="text-3xl text-[#da924e] text-center font-bold">
+				<div className="text-2xl text-[#da924e] text-center font-bold mt-2 mb-1">
 					Bangladesh Railway
 				</div>
-				<div className="text-sm text-[#006747] text-center">
-					নিরাপদ<span>.</span>আরামদায়ক
-					<span>.</span>সাশ্রয়ী
+				<div className="text-[10px] text-[#006747] text-center flex justify-center items-center">
+					নিরাপদ
+					<div className="w-1 h-0.75 bg-[#006747] rounded-lg m-2"></div>
+					আরামদায়ক
+					<div className="w-1 h-0.75 bg-[#006747] rounded-lg m-2"></div>
+					সাশ্রয়ী
 				</div>
 			</div>
 		</>
--- popup/pages/Home.tsx
+++ popup/pages/Home.tsx
@@ -3,11 +3,11 @@ import Logo from "../components/Logo";
 export default function Home() {
 	return (
 		<>
-			<div className="flex flex-col items-center justify-center h-[100vh] bg-[#e4eae8]">
+			<div className="flex flex-col items-center justify-evenly h-[100vh] bg-[#e4eae8]">
 				<Logo />
-				<div className="text-center text-[#804600]">
+				<div className="text-center text-[#804600] flex justify-center items-center">
 					<a href="">Automations</a>
-					<span>.</span>
+					<div className="w-1 h-0.75 bg-[#804600] rounded-lg m-2 opacity-75"></div>
 					<a href="">Departures</a>
 				</div>
 			</div>
